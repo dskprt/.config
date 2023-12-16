6d039a97143f22fdfc1a7ec425ac5b779b986704
dskprt <14883902-roseri@users.noreply.gitlab.com>
2023-12-16 11:39:41 +0100

    re

--- user/contingency.nix
+++ user/contingency.nix
@@ -1,9 +1,12 @@
 { pkgs, ... }: {
 	users.users.contingency = {
 		uid = 100;
+		group = "contingency";
 		isSystemUser = true;
 		home = "/home/heart";
 		useDefaultShell = true;
 		createHome = true;
 	};
+
+	users.groups.contingency = {};
 }
